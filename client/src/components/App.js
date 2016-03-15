ef6383c954f7e38ed8028e3edeb4a5e14d19c89c
David Bergstrom <stigdavidbergstrom@gmail.com>
2016-03-15 16:54:11 +0100

    Axel component for experimenting

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Axel from './Axel'
 
 class App extends React.Component {
   constructor(props) {
@@ -8,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        App
+        <Axel />
       </div>
     )
   }
---
+++ client/src/components/Axel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Axel extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        Här kan du lägga in vad som helst typ :) 
+        {/* JavaScript innanför sådana här */}
+      </div>
+    )
+  }
+
+}
+
+export default Axel

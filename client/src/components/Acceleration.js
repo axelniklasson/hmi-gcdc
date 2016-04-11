3495b237183f512f25e8b5edc53f9303ad60cf26
David Bergstrom <stigdavidbergstrom@gmail.com>
2016-04-11 15:13:28 +0200

    Acceleration basic bar

--- client/src/components/Acceleration.js
+++ client/src/components/Acceleration.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {findDOMNode} from 'react-dom'
+import createjs from 'createjs-collection'
 import CSSModules from 'react-css-modules'
 import styles from '../styles/Acceleration'
 
@@ -7,15 +9,52 @@ class Acceleration extends Component {
     super(props)
   }
 
+  componentDidMount() {
+    let canvas = findDOMNode(this.refs.canvas)
+    this.stage = new createjs.Stage(canvas)
+  }
+
+  draw(acceleration) {
+    this.stage.removeAllChildren()
+
+    // Scale the acceleration to present pixels
+    let scaledAcc = acceleration * 25
+
+    // Draw white background
+    let base = new createjs.Shape()
+    base.graphics.beginFill('White').drawRect(80,100,140,10)
+    this.stage.addChild(base)
+
+    // Draw Acc or Dec
+    if (acceleration > 0) {
+      let acc = new createjs.Shape()
+      acc.graphics.beginFill('Green').drawRect(150,100,scaledAcc,10)
+      this.stage.addChild(acc)
+    } else if (acceleration < 0) {
+      let scaledDec = scaledAcc * (-1)
+      let dec = new createjs.Shape()
+      base.graphics.beginFill('Red').drawRect(150-scaledDec,100,scaledDec,10)
+      this.stage.addChild(dec)
+    }
+
+    this.stage.update()
+  }
+
   render() {
-    var { acceleration } = this.props;
+    let acceleration = this.props.acceleration
+
+    if(acceleration) {
+      this.draw(acceleration)
+    }
 
     return (
       <div styleName="container">
         <div styleName="circle">
-          <div styleName="text-box">
-            <div styleName="acceleration">{acceleration}</div>
-          </div>
+          <canvas
+            className="container"
+            ref="canvas"
+            width={244}
+            height={131} />
         </div>
       </div>
     )

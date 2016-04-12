e3877cde5b7a7a4dfacd1a79aef6b3599b2bd7c3
David Bergstrom <stigdavidbergstrom@gmail.com>
2016-04-12 13:25:39 +0200

    Removed commented rows

--- client/src/components/Acceleration.js
+++ client/src/components/Acceleration.js
@@ -35,7 +35,7 @@ class Acceleration extends Component {
         var color = "rgb(";
         color = color + red;
         color = color + ",0,0)";
-        console.log(color);
+
             let innerLine = new createjs.Shape();
             innerLine.graphics.beginStroke(color);
             innerLine.graphics.arc(150,150,i,-Math.PI/2,-Math.PI/2 + (acceleration*12.5*Math.PI/180), true);
@@ -43,31 +43,6 @@ class Acceleration extends Component {
             red = red-3;
       }
     }
-
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("white");
-    circle.graphics.drawCircle(150,150,10);
-    this.stage.addChild(circle);
-
-    // Scale the acceleration to present pixels
-    //let scaledAcc = acceleration * 25
-
-    // Draw white background
-    //let base = new createjs.Shape()
-    //base.graphics.beginFill('White').drawRect(80,100,140,10)
-    //this.stage.addChild(base)
-
-    // Draw Acc or Dec
-    //if (acceleration > 0) {
-     // let acc = new createjs.Shape()
-    //  acc.graphics.beginFill('Green').drawRect(150,100,scaledAcc,10)
-    //  this.stage.addChild(acc)
-    //} else if (acceleration < 0) {
-    //  let scaledDec = scaledAcc * (-1)
-     // let dec = new createjs.Shape()
-     // base.graphics.beginFill('Red').drawRect(150-scaledDec,100,scaledDec,10)
-     // this.stage.addChild(dec)
-    //}
 
     this.stage.update()
   }

91fb01c49562c24b3b20e5766daf035e3f4bb309
Axel Niklasson <axel.niklasson@live.com>
2016-04-12 14:58:27 +0200

    Added webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
           'style?sourceMap',
           'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
         ]
+      },
+      {
+        test: /\.(png|jpg)$/, 
+        loader: 'file?name=[path][name].[hash].[ext]'
       }
     ]
   },
